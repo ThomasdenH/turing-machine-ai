9a64d0e63e7ed2afba806b05abaaa193ea2b70ad
Thomas <denhollander.thomas@gmail.com>
2023-09-10 17:25:14 +0200

    More tests

--- tests/booklet.rs
+++ tests/booklet.rs
@@ -7,6 +7,35 @@ use turing_machine_ai::{
     gametree::{Move, State},
 };
 
+fn test_game(verifiers: &[usize], moves: &[Move], solution: Code) -> Result<(), Box<dyn Error>> {
+    let game = Game::new_from_verifier_numbers(verifiers.iter().copied());
+    let possible_solutions = game.possible_solutions();
+    let mut state = State::new(&game, (&possible_solutions).into());
+
+    let actual_codes_guessed = moves
+        .iter()
+        .filter(|expected_move| matches!(expected_move, Move::ChooseNewCode(_)))
+        .count();
+    let actual_verifiers_checked = moves
+        .iter()
+        .filter(|expected_move| matches!(expected_move, Move::ChooseVerifier(_)))
+        .count();
+
+    for next_move in moves {
+        if !matches!(next_move, Move::VerifierSolution(_)) {
+            let (game_score, best_next_move) = state.find_best_move();
+            assert!(actual_codes_guessed == usize::from(game_score.codes_guessed));
+            assert!(actual_verifiers_checked == usize::from(game_score.verifiers_checked));
+            assert_eq!(*next_move, best_next_move);
+        }
+        state = state.after_move(*next_move)?.0;
+    }
+
+    assert!(state.is_solved());
+    assert_eq!(state.solution(), Some(solution));
+    Ok(())
+}
+
 #[test]
 fn test_01() -> Result<(), Box<dyn Error>> {
     let game = Game::new_from_verifier_numbers([4, 9, 11, 14].iter().copied());
@@ -38,7 +67,6 @@ fn test_02() -> Result<(), Box<dyn Error>> {
     let game = Game::new_from_verifier_numbers([3, 7, 10, 14].iter().copied());
     let possible_solutions = game.possible_solutions();
     let state = State::new(&game, (&possible_solutions).into());
-    dbg!(state.possible_solutions());
 
     let (_, next_move) = state.find_best_move();
     assert_eq!(next_move, Move::ChooseNewCode(Code::from_digits(2, 3, 1)?));
@@ -60,4 +88,47 @@ fn test_02() -> Result<(), Box<dyn Error>> {
     assert_eq!(state.solution(), Some(Code::from_digits(4, 3, 5)?));
 
     Ok(())
+}
+
+#[test]
+fn test_03() -> Result<(), Box<dyn Error>> {
+    test_game(
+        &[4, 9, 13, 17],
+        &[
+            Move::ChooseNewCode(Code::from_digits(1, 1, 1)?),
+            Move::ChooseVerifier(2.into()),
+            Move::VerifierSolution(Cross),
+            Move::ChooseVerifier(3.into()),
+            Move::VerifierSolution(Check),
+        ],
+        Code::from_digits(3, 3, 1)?,
+    )
+}
+
+#[test]
+fn test_04() -> Result<(), Box<dyn Error>> {
+    test_game(
+        &[3, 8, 15, 16],
+        &[
+            Move::ChooseNewCode(Code::from_digits(2, 1, 1)?),
+            Move::ChooseVerifier(0.into()),
+            Move::VerifierSolution(Cross),
+            Move::ChooseVerifier(2.into()),
+            Move::VerifierSolution(Cross),
+        ],
+        Code::from_digits(3, 4, 5)?,
+    )
+}
+
+#[test]
+fn test_05() -> Result<(), Box<dyn Error>> {
+    test_game(
+        &[2, 6, 14, 17],
+        &[
+            Move::ChooseNewCode(Code::from_digits(1, 1, 1)?),
+            Move::ChooseVerifier(1.into()),
+            Move::VerifierSolution(Check),
+        ],
+        Code::from_digits(3, 5, 4)?,
+    )
 }
